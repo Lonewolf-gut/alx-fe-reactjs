39a7797b6ba8e095a2321231a2fcc3f81d7a12c2
Lonewolf-gut <elishaagbi04@gmail.com>
2025-02-13 22:51:55 +0000

    ADDED

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import Header from "./Header";
 import MainContent from "./MainContent";
 import Footer from "./Footer";
 import "./App.css";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,6 +16,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </>
   );
 }
