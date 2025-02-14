dd44ab7ee5e936e746b6df4e9cbe789a87e64229
Agbi Elisha <73792752+Lonewolf-gut@users.noreply.github.com>
2025-02-14 09:25:39 +0000

    Update App.jsx
    Added the welcomemessage jsx file again

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-//import WelcomeMessage from "./components/WelcomeMess
+import WelcomeMessage from "./components/WelcomeMessage;
 import Header from "./Header";
 import MainContent from "./MainContent";
 import Footer from "./Footer";
@@ -13,6 +13,7 @@ function App() {
 
   return (
     <>
+      <WelcomeMessage/>
       <Header />
       <MainContent />
       <Footer />
