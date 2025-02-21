14a172f71377a25a0d4a0352d449c49c4ad1bf94
Lonewolf-gut <elishaagbi04@gmail.com>
2025-02-21 20:55:21 +0000

    user context

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,11 +1,16 @@
-const UserProfile = (props) => {
+import { useContext } from "react";
+import UserContext from "../UserContext";
+
+const UserProfile = () => {
+  const user = useContext(UserContext);
+
   return (
     <div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
-      <h2 style={{ color: "blue" }}>{props.name}</h2>
+      <h2 style={{ color: "blue" }}>{user.name}</h2>
       <p>
-        Age: <span style={{ fontWeight: "bold" }}>{props.age}</span>
+        Age: <span style={{ fontWeight: "bold" }}>{user.age}</span>
       </p>
-      <p>Bio: {props.bio}</p>
+      <p>Bio: {user.bio}</p>
     </div>
   );
 };
