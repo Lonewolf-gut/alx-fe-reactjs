16c7dcbcff863992dda4e4c0df471191cf2396d3
Lonewolf-gut <elishaagbi04@gmail.com>
2025-02-21 20:17:46 +0000

    new something

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+//import WelcomeMessage from "./components/WelcomeMess
+import Header from "./Header";
+import MainContent from "./MainContent";
+import Footer from "./Footer";
+import "./App.css";
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </>
+  );
+}
+
+export default App;
