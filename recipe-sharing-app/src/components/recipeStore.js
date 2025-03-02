1c793d291f23ae56445a9ca1f8cb0361bd6e05e1
Lonewolf-gut <elishaagbi04@gmail.com>
2025-03-02 22:03:50 +0000

    all

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,7 +3,19 @@ import create from 'zustand';
 const useRecipeStore = create((set) => ({
   recipes: [],
   favorites: [],
-  
+  // Add a new recipe
+  addRecipe: (newRecipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, newRecipe],
+      filteredRecipes: [...state.filteredRecipes, newRecipe], // Update filtered list
+    })),
+
+  // Set initial recipes (useful when fetching from an API)
+  setRecipes: (recipes) =>
+    set(() => ({
+      recipes,
+      filteredRecipes: recipes, // Sync filtered recipes initially
+    })),
   addFavorite: (recipeId) =>
     set((state) => ({
       favorites: [...new Set([...state.favorites, recipeId])], // Prevent duplicates
