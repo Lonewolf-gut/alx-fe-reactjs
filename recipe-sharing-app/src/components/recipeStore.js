55cef04999ef236e14091174cbc85724af151506
Lonewolf-gut <elishaagbi04@gmail.com>
2025-03-02 20:54:48 +0000

    zustand

---
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -0,0 +1,14 @@
+import { create } from 'zustand';
+
+const useRecipeStore = create((set) => ({
+  recipes: [],
+  
+  // Action to add a new recipe
+  addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
+  // Action to set the initial list of recipes
+  setRecipes: (recipes) => set({ recipes }),
+}));
+
+export default useRecipeStore;
